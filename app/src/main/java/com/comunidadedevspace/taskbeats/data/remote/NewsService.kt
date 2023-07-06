152c27a3fe9ff55b50cf0c1286af01cd0d878f32
Bernardo Oechsler <bernardo.oechsler6@gmail.com>
2023-07-05 22:55:38 -0700

    Getting the news response

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/data/remote/NewsService.kt
@@ -0,0 +1,10 @@
+package com.comunidadedevspace.taskbeats.data.remote
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewsService {
+
+    @GET("news/category=science")
+    fun fetchNews(): Call<NewsResponse>
+}
